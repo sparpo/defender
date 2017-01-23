c651173c61826d2d47e4c5517490448e117e8ce6
Joey Corbett <joeyecorbett@gmail.com>
2017-01-23 23:59:56 +0000

    just testing some stuff

--- app/platforms/browser/www/js/index.js
+++ app/platforms/browser/www/js/index.js
@@ -1,4 +1,25 @@
-/*
+document.getElementById("setLocalStorage").addEventListener("click", setLocalStorage);
+document.getElementById("showLocalStorage").addEventListener("click", showLocalStorage);
+document.getElementById("removeProjectFromLocalStorage").addEventListener("click", removeProjectFromLocalStorage);
+document.getElementById("getLocalStorageByKey").addEventListener("click", getLocalStorageByKey);
+
+var localStorage = window.localStorage;	
+function setLocalStorage() {
+   localStorage.setItem("Name", "John");
+   localStorage.setItem("Job", "Developer");
+   localStorage.setItem("Project", "Cordova Project");
+}
+function showLocalStorage() {
+   console.log(localStorage.getItem("Name"));
+   console.log(localStorage.getItem("Job"));
+   console.log(localStorage.getItem("Project"));
+}	
+function removeProjectFromLocalStorage() {
+   localStorage.removeItem("Project");
+}
+function getLocalStorageByKey() {
+   console.log(localStorage.key(0));
+}
 var app = {
     // Application Constructor
     initialize: function() {
@@ -25,77 +46,5 @@ var app = {
         console.log('Received Event: ' + id);
     }
 };
-*/
 
-var controller = null;
-var app = {
-    // Application Constructor
-    initialize: function() {
-        if (navigator.userAgent.match(/(iPhone|iPod|iPad|Android|BlackBerry)/)) {
-            document.addEventListener("deviceready", this.onDeviceReady, false);
-        } else {
-            this.onDeviceReady();
-        }
-    },
-
-    onDeviceReady: function() {
-        var controller = new Controller();
-        // We will init / bootstrap our application here
-    },
-};
-var Controller = function() {
-    var controller = {
-        self: null,
-        initialize: function() {
-            self = this;
-            this.bindEvents();
-            self.renderSearchView(); 
-        },
-
-        bindEvents: function() {
-        	$('.tab-button').on('click', this.onTabClick);
-        },
-
-        onTabClick: function(e) {
-        	e.preventDefault();
-            if ($(this).hasClass('active')) {
-                return;
-            }
-        	
-            var tab = $(this).data('tab');
-            if (tab === '#add-tab') {
-                self.renderPostView();
-            } else {
-                self.renderSearchView();
-            }
-        },
-
-        renderPostView: function() {
-            $('.tab-button').removeClass('active');
-            $('#post-tab-button').addClass('active');
-
-            var $tab = $('#tab-content');
-            $tab.empty();
-            $("#tab-content").load("./views/post-project-view.html", function(data) {
-                $('#tab-content').find('#post-project-form').on('submit', self.postProject);
-            }); 
-        },
-       
-        renderSearchView: function() {
-            $('.tab-button').removeClass('active');
-            $('#search-tab-button').addClass('active');
-
-            var $tab = $('#tab-content');
-            $tab.empty();
-
-            var $projectTemplate = null;
-            $("#tab-content").load("./views/search-project-view.html", function(data) {
-                $projectTemplate = $('.project').remove();
-                // Load projects here
-            }); 
-        }
-    }
-    controller.initialize();
-    return controller;
-}
 app.initialize();
