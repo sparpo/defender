e60ec48b263666eb1bb78ff105ffda10191c4c2c
Joey Corbett <joeyecorbett@gmail.com>
2017-03-09 00:21:43 +0000

    Getting ready to add different pages. Still havent fixed "no message in email" problem

--- app/platforms/android/assets/www/js/index.js
+++ app/platforms/android/assets/www/js/index.js
@@ -1,24 +1,32 @@
 document.getElementById("send").addEventListener("click", send);
+document.getElementById("confirm").addEventListener("confirm", send);
 
 var localStorage = window.localStorage;	
+function confirm() {
+    localStorage.setItem("AdminsEmail", document.getElementById('emailBox').value);
+    console.log(localStorage.getItem("AdminsEmail"));
+}
 function send() {
    localStorage.setItem("Message", document.getElementById('textBox').value);
-   console.log(localStorage.getItem("Message"));
     var message = localStorage.getItem("Message");
+    console.log(message);
     var key = "dJdJekCVAFIqvUJ13DEczZjgIh_4MyeIGEHz2GBYKFe"; // << YOUR KEY HERE
     var message_name = "defender_send_message";    // << YOUR MESSAGE NAME HERE
     var url = "https://maker.ifttt.com/trigger/" + message_name + "/with/key/" + key;
     $.ajax({  
       url: url,
-      data: { value1: message },
-      data: { value2: "joeyecorbett@gmail.com"},
+      data: {value1: message},
+      data: {value2: "joeyecorbett@gmail.com"},
       dataType: "jsonp",
       complete: function(jqXHR, textStatus) {
-        alert("Message Sent"); 
+        console.log("Message Sent");
       } 
     });   
 }
-
+function confirm() {
+    localStorage.setItem("AdminsEmail", document.getElementById('emailBox').value);
+    console.log(localStorage.getItem("AdminsEmail"));
+}
 //http://defender.netne.net/sendmail.php?p=5kxP2wba&to=defender%40ricecollege.ie&subject=5th&body=hello
 var app = {
     // Application Constructor
